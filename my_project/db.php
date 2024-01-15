d72a8d4cc28a861c4769f5ad69a549ce4b22fc3e
mti7086 <58331499+mti7086@users.noreply.github.com>
2024-01-16 01:52:46 +0900

    Update db.php

--- my_project/db.php
+++ my_project/db.php
@@ -4,7 +4,7 @@ $dbPassword = '1234';
 $dbName = 'sqlDB';
 
 function connectDB() {
-    $con = mysqli_connect("localhost", "root", "1234", "sqlDB");
+    $con = mysqli_connect("localhost", "$dbUser", "$dbPassword", "$dbName");
     if (!$con) {
         die("MySQL 접속 실패: " . mysqli_connect_error());
     }
